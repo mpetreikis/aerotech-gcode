0cdd48472a2e20a620981a2c3d460d96f2a39374
Matas Petreikis <mpetreikis@tutanota.com>
2024-11-20 10:41:20 +0000

    added the g-code convertion and multiplication scripts

---
+++ conversion_v2.py
@@ -0,0 +1,177 @@
+import pandas as pd
+
+# reading off the text from the printing programme into an array
+full_text = pd.DataFrame(
+    [line.strip().split(" ")
+     for line in open("PDMS_838um unedited.txt", encoding="utf8")]
+)
+
+############################################################
+
+# Write a function that removes  a row of a dataframe with
+# a specific starting symbol in one of its elements or keeps
+# all rows starting with a certain symbol
+
+
+def kick_keep(text, symbols, keep):
+    """Kicking or keeping rows with elements with certain symbols"""
+    remaining = ~(text == True)  # Creating a dataframe of True
+    remaining = remaining.any(axis=1)  # Creating a Series of True
+    for i in symbols:
+        # indices and columns that have the symbol
+        indices = text.stack().str.contains(
+            i,
+            case=True,
+        )
+        # Checking which of the rows should be considered
+        indices = indices.unstack().any(axis=1)
+        if keep == True:
+            remaining = remaining & indices
+        elif keep == False:
+            indices = ~indices
+            remaining = remaining & indices
+    text = text.loc[remaining]
+    return text
+
+
+############################################################
+
+# Write a function that replaced wanted string elements with
+# other string elements
+
+
+def lets_replace(text, symbols, repl_symbols):
+    for a, b in zip(symbols, repl_symbols):
+        text = text.stack().str.replace(a, b).unstack()
+    return text
+
+
+############################################################
+
+# Write a function that determines the "high" and "low" positions
+# of the nozzle; "high" being when it is not touching the substrate,
+# and "low" when it is. Luckily, when Flatcam generates a Gcode text
+# file, it assumes that when the printing head is moving to a "low"
+# position, it will have to mill the material. Therefore, G01 code is
+# used in that case and that is the easiest way of determining, which
+# position is "high" and which one is "low".
+
+
+def high_low(text):
+    z_indices = text[1].str.contains(
+        "Z",
+        case=True,
+    )
+    text_z = text.loc[z_indices, :]
+    g00_indices = text_z[0].str.contains(
+        "G00",
+        case=True,
+    )
+    g00_indices = g00_indices[g00_indices].index
+    g01_indices = text_z[0].str.contains(
+        "G01",
+        case=True,
+    )
+    g01_indices = g01_indices[g01_indices].index
+    high_z = text_z.loc[g00_indices[0], 1]
+    low_z = text_z.loc[g01_indices[0], 1]
+    high_low_ans = [high_z, low_z]
+    return high_low_ans
+
+
+############################################################
+
+# Need to keep everything that has G0 start:
+full_text = kick_keep(full_text, ["G0"], keep=True)
+
+# Need to remove the lines with max Z value (Z15) and all of the F codes
+full_text = kick_keep(full_text, ["Z15", "F"], keep=False)
+
+# # Need to find the "high" and "low" positions of the nozzle
+high_low_list = high_low(full_text)
+top_z = high_low_list[0]
+bottom_z = high_low_list[1]
+
+# Finding out the parameters that will be needed for converting the code
+system = input(
+    "Is the code written in Imperial or Metric (default) units? Write 'imperial' or 'metric' without apostrophes and press Enter: "
+)
+speed = input(
+    "At what speed should the nozzle be moving, i.e. what is the feed rate? Please write a number and press Enter: "
+)
+top_position = input(
+    "Z-coordinate for printing (the highest point of the stage)? Please write a number or type 'd' without apostrophes for the default value and press Enter: "
+)
+bottom_position = input(
+    "Z-coordinate for moving the stage without printing (the lowest point of the stage)? Please write a number or type 'd' without apostrophes for the default value and press Enter: "
+)
+f_code = "F" + speed
+
+# Writing the cases for the preamble if the system is metric or imperial
+if system == "metric" or "Metric" or "m":
+    preamble_list = [
+        ["G90", None, None],
+        ["G71", None, None],
+        ["G94", None, None],
+        ["G01", f_code, None],
+        ["G00", "Z0.0000", None],
+    ]
+    preamble = pd.DataFrame(preamble_list)
+    if top_position == "d":
+        top_position = "17.5390"
+    if bottom_position == "d":
+        bottom_position = "16.5230"
+
+elif system == "imperial" or "Imperial" or "i":
+    preamble_list = [
+        ["G90", None, None],
+        ["G70", None, None],
+        ["G94", None, None],
+        ["G01", f_code, None],
+        ["G00", "Z0.0000", None],
+    ]
+    preamble = pd.DataFrame(preamble_list)
+    if top_position == "d":
+        top_position = "0.69051"
+    if bottom_position == "d":
+        bottom_position = "0.65051"
+else:
+    preamble_list = [
+        ["G90", None, None],
+        ["G71", None, None],
+        ["G94", None, None],
+        ["G01", f_code, None],
+        ["G00", "Z0.0000", None],
+    ]
+    preamble = pd.DataFrame(preamble_list)
+    if top_position == "d":
+        top_position = "17.5390"
+    if bottom_position == "d":
+        bottom_position = "16.5230"
+
+# Making the correct string for positions
+top_position = "Z" + top_position
+bottom_position = "Z" + bottom_position
+
+# Need to replace all "high" and "low" Z values with newer ones
+# In this case, the top position refers to the bottom z because
+# we are moving the stage rather than the nozzle (or the milling head)
+full_text = lets_replace(full_text, [top_z, bottom_z], [
+                         bottom_position, top_position])
+
+# Create an ending
+ending_list = [["G00", "Z0.0000", None], ["G00", "X0.0000", "Y0.0000"]]
+ending = pd.DataFrame(ending_list)
+
+# Concatenate all of the dataframes
+printing_programme = pd.concat(
+    [preamble, full_text, ending], ignore_index=True, axis=0)
+
+# Write the dataframe to a text file
+printing_programme.to_csv(
+    "converted.txt",
+    header=None,
+    index=None,
+    sep=" ",
+    mode="a",
+)
---
+++ multilayer_printing.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+
+# reading off the text from the printing programme into an array
+full_text = pd.DataFrame([line.strip().split(" ")
+                         for line in open("Mould edited v2.txt")])
+# parsing through the text to find the indices of rows and the corresponding z-coordinates
+z_coordinates = full_text.stack()[
+    full_text.stack().str.contains("z", case=False)
+].unstack()
+# print(z_coordinates)
+
+# finding the first and last names of the rows with z-coordinates
+all_row_name = z_coordinates.index.values.tolist()  # a list of values of every row
+first_row_name = all_row_name[0]  # value of the row of the first z-coordinate
+last_row_name = all_row_name[-1]  # value of the row of the last z-coordinate
+
+# cutting out the whole beginning before the first z-coordinate
+preamble = full_text.loc[0: first_row_name - 1]
+
+# cutting out the ending of the code where we home the stage to (0,0,0)
+ending = full_text.loc[last_row_name:]
+
+# cutting out only the chunk of text that will be updated
+single_layer = full_text.loc[first_row_name: last_row_name - 1]
+# print(single_layer)
+
+# adjusting z_coordinates to remove the Z0.0000 row because that row is not going to be iterated
+z_coordinates = z_coordinates.drop(last_row_name)
+
+# asking for a specific number of layers
+no_layers = int(
+    input("Enter the number of layers of ink that you wish to print: "))
+
+# asking for the incremental shift in z-axis position with every layer
+z_increment = np.float64(
+    input("Enter the incremental shift in the z-axis position: "))
+
+# creating a dataframe for the total printing programme
+main_text = single_layer.stack().unstack()
+# stack and unstack functions are used so that an actual copy is made, otherwise the copy is made only when main_text is
+# called and that is after the adjustment to the coordinates
+
+# striping the z coordinates of the 'Z'
+z_coordinates = pd.DataFrame(z_coordinates.squeeze().str.replace("Z", ""))
+
+# converting the z coordinates from strings to float numbers
+z_coordinates = pd.DataFrame(pd.to_numeric(z_coordinates.squeeze()))
+
+# need to know which elements in the array need to be replaced in the upcoming for loop
+z_coordinates_rows = z_coordinates.index.values.tolist()
+
+# just some code I am writing for the sake of testing out how concatenation works and finding out what is wrong with the
+# the code I have written so far
+
+# let's do the first shift in the z-coordinates
+# new_block_z_1=pd.DataFrame(z_coordinates.squeeze()-z_increment)
+# new_block_z_1=pd.DataFrame('Z' + new_block_z_1.squeeze().astype(str))
+# single_layer.loc[z_coordinates_rows,1]=new_block_z_1.squeeze()
+# print(single_layer)
+
+# let's concatenate this first bit with the bit after the first shift
+# main_text=pd.concat([main_text,single_layer],ignore_index=True,axis=0)
+# print(main_text)
+
+# creating a loop to update the z-coordinates for every layer and add the block for every layer to the total printing programme
+for i in range(1, no_layers):
+    new_block_z = pd.DataFrame(
+        round((z_coordinates.squeeze() - z_increment * i), 5)
+    )  # updating the z-coordinates
+    new_block_z = pd.DataFrame(
+        "Z" + new_block_z.squeeze().astype(str)
+    )  # adding Z in the string
+    single_layer.loc[
+        z_coordinates_rows, 1
+    ] = new_block_z.squeeze()  # replacing the Z-coordinates in the single layer block
+    main_text = pd.concat(
+        [main_text, single_layer], ignore_index=True, axis=0
+    )  # stacking the new block under the code we have so far
+
+# stacking all the needed building blocks to create the full text for the printing programme
+printing_programme = pd.concat(
+    [preamble, main_text, ending], ignore_index=True, axis=0)
+print(printing_programme)
+printing_programme.to_csv("printing.txt", header=None,
+                          index=None, sep=" ", mode="a")
